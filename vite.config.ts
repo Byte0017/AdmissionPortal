caa281130d8cfc8416215e9368ab84113487910d
Ajay Yadav <141677772+Byte0017@users.noreply.github.com>
2025-02-25 20:44:00 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/AdmissionProtal',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
